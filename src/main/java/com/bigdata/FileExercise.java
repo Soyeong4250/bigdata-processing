dfdb1a31d8db894eaecd4ae211a10d9d0676d3de
이소영 <th42500@naver.com>
2022-10-06 22:49:12 +0900

    [Refactor] readChars() 추가 및 생성자를 통한 filename 저장

--- src/main/java/com/bigdata/FileExercise.java
+++ src/main/java/com/bigdata/FileExercise.java
@@ -1,8 +1,17 @@
 package com.bigdata;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileExercise {
+    private String filename;
+
+    public FileExercise(String filename) {
+        this.filename = filename;
+    }
+
     private void exploreDirectory() {
         File dir = new File("./");
         File[] files = dir.listFiles();
@@ -11,13 +20,35 @@ public class FileExercise {
             System.out.println(file);
         }
     }
+    private char readAChar() throws IOException {
+        FileReader fileReader = new FileReader(this.filename);
+        return (char)fileReader.read();
+    }
 
-    public static void main(String[] args) {
-        FileExercise fileExercise = new FileExercise();
+    private String readChars(int N) throws IOException {
+        FileReader fileReader = new FileReader(this.filename);
+        StringBuilder sb  = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int ascii = fileReader.read();
+            if(ascii == -1) {  // 더 이상 읽을 글자가 없다면
+                return sb.toString();
+            }
+            sb.append((char)ascii);
+        }
+        return sb.toString();
+    }
 
-        fileExercise.exploreDirectory();
+    public static void main(String[] args) throws IOException {
+        FileExercise fileExercise = new FileExercise("./hello.txt");
 
-        
+        // fileExercise.exploreDirectory();
+
+        char ch = fileExercise.readAChar();
+        // System.out.println(ch);
+
+        String str = fileExercise.readChars(3);
+        System.out.println(str);
 
     }
+
 }
