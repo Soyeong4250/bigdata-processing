84e305c0a60d859e4c1f7dcb3a2e85971046977a
이소영 <th42500@naver.com>
2022-10-07 22:10:39 +0900

    [Remove] 대용량 파일 읽기기능 옮기기
    readLinesTest() 메서드 SchoolParserTest.java 👉 FileControllerTest.java 이동

--- src/test/java/com/bigdata/parser/FileControllerTest.java
+++ src/test/java/com/bigdata/parser/FileControllerTest.java
@@ -1,19 +1,21 @@
 package com.bigdata.parser;
 
 import com.bigdata.domain.School;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FileControllerTest {
     FileController<School> fileController;
     String path;
 
 
     @Test
+    @Order(1)
     @DisplayName("인코딩 확인 테스트")
     void findFlieEncoding() throws IOException {
         path = "C:\\pjt\\전국초중등학교위치표준데이터.csv";
@@ -24,4 +26,15 @@ class FileControllerTest {
         assertNotEquals("UTF-8", encoding);
     }
 
+    @Test
+    @Order(2)
+    @DisplayName("대용량 데이터 파일을 잘 읽어오는지 테스트")
+    void readLinesTest() throws IOException {
+        FileController<School> schoolFileController = new FileController<>(new SchoolParser());
+        String filename = "C:\\pjt\\전국초중등학교위치표준데이터.csv";
+        String encoding = schoolFileController.findFileEncoding(filename);
+        List<School> schoolsList = schoolFileController.readLines(filename, encoding);
+
+        assertEquals(23948, schoolsList.size());
+    }
 }
