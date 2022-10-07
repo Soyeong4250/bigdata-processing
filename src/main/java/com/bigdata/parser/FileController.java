c2dee5f98fc4dd2938dde436829be7b21d06949f
이소영 <th42500@naver.com>
2022-10-07 19:46:13 +0900

    [Add] findFileEncoding() 메서드 추가
    csv 파일 인코딩 확인 기능 추가

--- src/main/java/com/bigdata/parser/FileController.java
+++ src/main/java/com/bigdata/parser/FileController.java
@@ -1,7 +1,8 @@
 package com.bigdata.parser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import org.mozilla.universalchardet.UniversalDetector;
+
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,11 +17,38 @@ public class FileController<T> {
         this.parser = parser;
     }
 
-    public List<T> readLines(String filename) throws IOException {
+    public String findFileEncoding(String path) throws IOException {
+        byte[] buf = new byte[4096];
+        FileInputStream fis = new FileInputStream(new File(path));
+
+        UniversalDetector detector = new UniversalDetector(null);
+
+        int bufSize;
+        while ((bufSize = fis.read(buf)) > 0 && !detector.isDone()) {
+            detector.handleData(buf, 0, bufSize);
+        }
+
+        detector.dataEnd();
+
+        String encoding = detector.getDetectedCharset();
+        if (encoding != null) {
+            System.out.println("Detected encoding = " + encoding);
+        } else {
+            System.out.println("No encoding detected.");
+        }
+
+        detector.reset();
+
+        return encoding;
+    }
+
+    public List<T> readLines(String path) throws IOException {
         List<T> fileContents = new ArrayList<>();
 
         BufferedReader br;
-        br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8);
+
+
+        br = Files.newBufferedReader(Paths.get(path), StandardCharsets.UTF_8);
         String line;
         br.readLine();
         while((line = br.readLine()) != null) {
