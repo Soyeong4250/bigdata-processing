7d1d7cdb1b555cfdeb9824e54d635db907f1fd1b
이소영 <th42500@naver.com>
2022-11-05 20:15:41 +0900

    [Add] School 클래스  추가

---
+++ src/main/java/com/bigdata/domain/School.java
@@ -0,0 +1,12 @@
+package com.bigdata.domain;
+
+import java.time.LocalDate;
+
+public class School {
+    private int id;
+    private String schoolName;
+    private String schoolGrade;
+    private LocalDate establishmentDate;
+    private String establishType;
+    private
+}
