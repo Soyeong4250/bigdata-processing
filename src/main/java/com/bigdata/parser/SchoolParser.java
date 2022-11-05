7d46f869677b41c801f09d1764d51da977d3f808
이소영 <th42500@naver.com>
2022-11-05 22:29:24 +0900

    [Add] SchoolParser 클래스 추가
    데이터를 읽어 각 행을 School 객체로 변환하는 메서드 구현

--- src/main/java/com/bigdata/parser/SchoolParser.java
+++ src/main/java/com/bigdata/parser/SchoolParser.java
@@ -2,12 +2,21 @@ package com.bigdata.parser;
 
 import com.bigdata.domain.School;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 public class SchoolParser implements Parser<School>{
 
     @Override
     public School parse(String str) {
-        System.out.println(str);
-
-        return new School();
+        String[] splitted = str.split(",");
+        System.out.println(Arrays.toString(splitted));
+        String date = splitted[3];
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+        boolean isMainSchool = (splitted[5].equals("본교")) ? true : false;
+        return new School(splitted[0], splitted[1], splitted[2], LocalDateTime.of(year, month, day,0, 0, 0), splitted[4], isMainSchool, splitted[6],
+                splitted[7], splitted[8], splitted[9], splitted[10], splitted[11], splitted[12], Double.parseDouble(splitted[15]), Double.parseDouble(splitted[16]));
     }
 }
