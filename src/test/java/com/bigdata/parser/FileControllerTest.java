14816f19c4429fdc08fa680f3f3344d1716a27e9
이소영 <th42500@naver.com>
2022-11-06 02:23:48 +0900

    [Add] 새로운 파일 생성 및 SQL INSERT문 저장 테스트
    데이터 중 학교 도로명 주소 컬럼 내에 ","가 존재하는 데이터가 있어 데이터가 잘못 분리되어 NumberFormateException 발생

--- src/test/java/com/bigdata/parser/FileControllerTest.java
+++ src/test/java/com/bigdata/parser/FileControllerTest.java
@@ -3,6 +3,7 @@ package com.bigdata.parser;
 import com.bigdata.domain.School;
 import org.junit.jupiter.api.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class FileControllerTest {
     FileController<School> fileController;
     String path;
-
+    List<School> schoolsList;
 
     @Test
     @Order(1)
@@ -30,11 +31,34 @@ class FileControllerTest {
     @Order(2)
     @DisplayName("대용량 데이터 파일을 잘 읽어오는지 테스트")
     void readLinesTest() throws IOException {
-        FileController<School> schoolFileController = new FileController<>(new SchoolParser());
+        fileController = new FileController<>(new SchoolParser());
         String filename = "C:\\PJT\\전국초중등학교위치표준데이터.csv";
-        String encoding = schoolFileController.findFileEncoding(filename);
-        List<School> schoolsList = schoolFileController.readLines(filename, encoding);
+        String encoding = fileController.findFileEncoding(filename);
+        schoolsList = fileController.readLines(filename, encoding);
 
         assertEquals(23948, schoolsList.size());
+    }
+
+    @Test
+    @DisplayName("새로운 파일 생성하여 SQL INSERT문 저장하기")
+    void createFileAndWriteSqlQueries() throws IOException {
+        fileController = new FileController<>(new SchoolParser());
+        String path = "C:\\PJT\\전국초중등학교위치표준데이터.csv";
+        schoolsList = fileController.readLines(path, "EUC-KR");
+
+        StringBuilder quries = new StringBuilder();
+        for (School school : schoolsList) {
+            if(quries.length() == 0) {
+                quries.append(school.getSqlInsertQuery());
+            } else {
+                quries.append(school.getTupleString());
+            }
+        }
+
+        String newFileName = "school_insert.sql";
+        fileController.createNewSqlFile(newFileName);
+        fileController.writeContents(quries.toString(), newFileName);
+
+        assertEquals(1, new File(newFileName).exists());
     }
 }
